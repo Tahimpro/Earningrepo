bdda5d0507d82d9fbffbe0b46afe70c7f9c1ec30
TG-MoviesHouse8 <118811792+TG-MoviesHouse8@users.noreply.github.com>
2023-05-09 21:04:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyberTG/TOM-BOT.git /TOM-BOT
+  git clone https://github.com/Tahimpro/Earningrepo.git /Earningrepo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TOM-BOT
+  git clone $UPSTREAM_REPO /Earningrepo
 fi
 cd /TOM-BOT
 pip3 install -U -r requirements.txt
